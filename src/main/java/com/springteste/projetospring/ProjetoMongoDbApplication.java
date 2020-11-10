20225cfe2d21cee761c3eda80f9dabb3f34379e2
Pedro Henrique Fernandes Santos <pedro.h.santos@basis.com.br>
2020-11-10 14:44:22 -0300

    Primeiro commit

---
+++ src/main/java/com/springteste/projetospring/ProjetoMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.springteste.projetospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoMongoDbApplication.class, args);
+	}
+
+}
