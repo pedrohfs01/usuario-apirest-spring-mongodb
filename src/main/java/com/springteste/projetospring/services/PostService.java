85b284a381dfdb6d3f5fc68942dcabee7e952488
Pedro Henrique Fernandes Santos <pedro.h.santos@basis.com.br>
2020-11-11 13:12:57 -0300

    Obtendo post por id

---
+++ src/main/java/com/springteste/projetospring/services/PostService.java
@@ -0,0 +1,22 @@
+package com.springteste.projetospring.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springteste.projetospring.domain.Post;
+import com.springteste.projetospring.repositories.PostRepository;
+import com.springteste.projetospring.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+}
