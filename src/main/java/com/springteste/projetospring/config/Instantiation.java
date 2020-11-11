bea734e08b4c84d7472f293527ad53e60dc5f211
Pedro Henrique Fernandes Santos <pedro.h.santos@basis.com.br>
2020-11-11 13:36:09 -0300

    Adicionado comentários aos posts

--- src/main/java/com/springteste/projetospring/config/Instantiation.java
+++ src/main/java/com/springteste/projetospring/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import com.springteste.projetospring.domain.Post;
 import com.springteste.projetospring.domain.User;
 import com.springteste.projetospring.dto.AuthorDTO;
+import com.springteste.projetospring.dto.CommentDTO;
 import com.springteste.projetospring.repositories.PostRepository;
 import com.springteste.projetospring.repositories.UserRepository;
 
@@ -33,14 +34,23 @@ public class Instantiation implements CommandLineRunner {
 		User bob = new User(null, "BobGrey", "bob@gmail.com");
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
-
 		
 		Post p1 = new Post(null, Instant.parse("2017-12-25T20:30:50Z"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post p2 = new Post(null, Instant.parse("2018-03-23T15:45:19Z"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", Instant.parse("2018-03-21T20:11:50Z"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", Instant.parse("2018-03-24T10:19:59Z"), new AuthorDTO(bob));
+		
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", Instant.parse("2018-02-20T15:33:12Z"), new AuthorDTO(alex));
+		
+		p1.getComments().addAll(Arrays.asList(c1,c2));
+		p2.getComments().add(c3);
 		
 		postRepository.saveAll(Arrays.asList(p1, p2));
 		
 		maria.getPosts().addAll(Arrays.asList(p1,p2));
 		userRepository.save(maria);
+		
+		
 	}
 }
