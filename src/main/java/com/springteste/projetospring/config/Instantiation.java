4ec92bca0d43867d705bc47e510f31ac031a756b
Pedro Henrique Fernandes Santos <pedro.h.santos@basis.com.br>
2020-11-11 12:29:47 -0300

    Adicionado Post e o Repository respectivo

--- src/main/java/com/springteste/projetospring/config/Instantiation.java
+++ src/main/java/com/springteste/projetospring/config/Instantiation.java
@@ -1,12 +1,15 @@
 package com.springteste.projetospring.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.springteste.projetospring.domain.Post;
 import com.springteste.projetospring.domain.User;
+import com.springteste.projetospring.repositories.PostRepository;
 import com.springteste.projetospring.repositories.UserRepository;
 
 @Configuration
@@ -15,15 +18,23 @@ public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "BobGrey", "bob@gmail.com");
 		
+		Post p1 = new Post(null, Instant.parse("2017-12-25T20:30:50Z"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post p2 = new Post(null, Instant.parse("2018-03-23T15:45:19Z"), "Bom dia", "Acordei feliz hoje!", maria);
+		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(p1, p2));
 	}
 }
